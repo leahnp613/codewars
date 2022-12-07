c595c4cb31e8ac1e535f452081864ea742f8752f
leahnp613 <leahnagarpowers@gmail.com>
2022-12-07 17:06:09 -0500

    Looping arrays with 2 variables

---
+++ CategorizeNewMember.js
@@ -0,0 +1,8 @@
+function openOrSenior(data){
+   return data.map((age,handicap) =>{ 
+    if (age >= 55 && handicap > 7) {
+        return "Senior"
+     } else { return "Open"
+     }
+   }) // 
+  }
