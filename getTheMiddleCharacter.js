21c50a552f6cbde4c9c9452265dee78b8fa06e53
leahnp613 <leahnagarpowers@gmail.com>
2022-12-07 18:51:39 -0500

    Getting stuck on slicing

---
+++ getTheMiddleCharacter.js
@@ -0,0 +1,9 @@
+function getMiddle(s){
+    if (s.length % 2 == 0) {
+      return s.substring(s.length/2-1, s.length + 2)
+    
+    }else return s.substring(s.length/2, s.length +1)
+    
+    
+    //Code goes here!
+  }
