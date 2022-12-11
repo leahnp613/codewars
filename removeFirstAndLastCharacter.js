569c1a471232c9451d9cfbf4ff0672dc7f5eaeac
leahnp613 <leahnagarpowers@gmail.com>
2022-12-11 11:42:47 -0500

    Focusing on returning the substring and length.

---
+++ removeFirstAndLastCharacter.js
@@ -0,0 +1,4 @@
+//It is pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You are given one parameter, the original string. You do not have to worry with strings with less than two characters.
+function removeChar(str){
+    return str = str.substring(1, str.length -1);
+    };
